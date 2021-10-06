4774e3aac480517c6f42e651efc50028ee5addde
Archy <archy.revera@protonmail.com>
2021-10-07 01:55:48 +0300

    Refactor shell sort

--- src/shell_sort.cpp
+++ src/shell_sort.cpp
@@ -1,22 +1,12 @@
 void shell_sort(int* arr, int size){
-    // Start with a big gap, then reduce the gap
-    for (int gap = size/2; gap > 0; gap /= 2) {
-        // Do a gapped insertion sort for this gap size.
-        // The first gap elements a[0..gap-1] are already in gapped order
-        // keep adding one more element until the entire array is
-        // gap sorted
-        for (int i = gap; i < size; i++) {
-            // add a[i] to the elements that have been gap sorted
-            // save a[i] in temp and make a hole at position i
+    for(int gap = size/2; gap > 0; gap /= 2){
+        for(int i = gap; i < size; i++){
             int temp = arr[i];
  
-            // shift earlier gap-sorted elements up until the correct
-            // location for a[i] is found
-            int j;
-            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
-                arr[j] = arr[j - gap];
+            int j = i;
+            for (; (j >= gap) && (temp < arr[j-gap]); j -= gap)
+                arr[j] = arr[j-gap];
              
-            //  put temp (the original a[i]) in its correct location
             arr[j] = temp;
         }
     }
@@ -24,24 +14,14 @@ void shell_sort(int* arr, int size){
 
 long shell_sort_verb(int* arr, int size){
     int comps = 0;
-    // Start with a big gap, then reduce the gap
-    for (int gap = size/2; gap > 0; gap /= 2, comps++) {
-        // Do a gapped insertion sort for this gap size.
-        // The first gap elements a[0..gap-1] are already in gapped order
-        // keep adding one more element until the entire array is
-        // gap sorted
-        for (int i = gap; i < size; i++, comps++) {
-            // add a[i] to the elements that have been gap sorted
-            // save a[i] in temp and make a hole at position i
+    for(int gap = size/2; gap > 0; gap /= 2, comps++){
+        for(int i = gap; i < size; i++, comps++){
             int temp = arr[i];
  
-            // shift earlier gap-sorted elements up until the correct
-            // location for a[i] is found
-            int j;           
-            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap, comps++)
-                arr[j] = arr[j - gap];
+            int j = i;
+            for(; (j >= gap) && (temp < arr[j-gap]); j -= gap, comps+=2)
+                arr[j] = arr[j-gap];
              
-            //  put temp (the original a[i]) in its correct location
             arr[j] = temp;
         }
     }
